0d510309fdcc833a672e34bf0d8cd4efeee46c71
Winter-end <persymon.n@gmail.com>
2025-03-12 15:41:10 +0100

    Added Pie Chart for Poland + not done bar for top20 countries by gold medal

--- olimpiada mat/imo.m
+++ olimpiada mat/imo.m
@@ -46,3 +46,55 @@ title('Number of Gold, Silver, Bronze Medals through Years');
 legend('Gold', 'Silver', 'Bronze', 'Location', 'northeast');
 xlim([min(years) max(years)]);
 hold off;
+
+
+
+df = readtable('Ranking of countries by Country.csv');
+
+data = df{100, 10:12};
+data
+
+figure(5);
+h = pie(data, {'Gold', 'Silver', 'Bronze'});
+
+txt = findobj(h, 'Type', 'text');
+
+if numel(txt) >= 3
+    txt(1).String = num2str(data(1));
+    txt(2).String = num2str(data(2));
+    txt(3).String = num2str(data(3));
+end
+
+gold   = [1, 0.84, 0];
+silver = [0.75, 0.75, 0.75];
+bronze = [0.8, 0.5, 0.2];
+
+patchHandles = findobj(h, 'Type', 'Patch');
+
+if numel(patchHandles) >= 3
+    patchHandles(1).FaceColor = gold;
+    patchHandles(2).FaceColor = silver;
+    patchHandles(3).FaceColor = bronze;
+end
+
+title('Medal Counts for Poland');
+
+
+countries = df{:, 2};
+medals = df{:, 10:12};
+
+[~, sortIdx] = sort(df{:, 10}, 'descend');
+sortedCountries = countries(sortIdx);
+sortedMedals = medals(sortIdx, :);
+
+top20Countries = sortedCountries(1:20);
+top20Medals = sortedMedals(1:20, :);
+
+top20CountriesCat = categorical(top20Countries);
+
+figure(6);
+bar(top20CountriesCat, top20Medals);
+xlabel('Countries');
+ylabel('Medal Count');
+title('Top 20 Countries by Gold Medals');
+legend('Gold', 'Silver', 'Bronze', 'Location', 'northeast');
